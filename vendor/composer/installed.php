0ac8ba908b072f3b2bfe3897280cec8afc0a86d5
BitPoet <the.bitpoet@gmail.com>
2025-11-19 11:40:26 +0100

    Update phpseclib, bump version to 0.1.3
    Update phpseclib to 2.0.49
    Bump version to 0.1.3
    Add supported PHP versions to README.md
    Update change log in README.md

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '31b7299fe334e3c51e0e437b535d1288f9f54cad',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,18 +11,18 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '31b7299fe334e3c51e0e437b535d1288f9f54cad',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'phpseclib/phpseclib' => array(
-            'pretty_version' => '2.0.47',
-            'version' => '2.0.47.0',
-            'reference' => 'b7d7d90ee7df7f33a664b4aea32d50a305d35adb',
+            'pretty_version' => '2.0.49',
+            'version' => '2.0.49.0',
+            'reference' => '4de468f48f0ab9709fc875aca0762abdc81cfa9b',
             'type' => 'library',
             'install_path' => __DIR__ . '/../phpseclib/phpseclib',
             'aliases' => array(),
